4e5db17302ee00d40a4523cbe1d24674ba19ed37
ujitha <dgujitha@gmail.com>
2014-01-31 09:31:20 +0530

    logic_testcase_home created

---
+++ resources/js/logic_testcase_home.js
@@ -0,0 +1,83 @@
+var allSelected = false;
+
+
+function isTestcaseSelected() {
+    var selected = false;
+    if (document.testcasesForm.WSO2_QAP_TEST_CASE_ID[0] != null) { // there is more than 1
+        for (var j = 0; j < document.testcasesForm.WSO2_QAP_TEST_CASE_ID.length; j++) {
+            selected = document.testcasesForm.WSO2_QAP_TEST_CASE_ID[j].checked;
+            if (selected) break;
+        }
+    } else if (document.testcasesForm.WSO2_QAP_TEST_CASE_ID != null) { // only 1
+        selected = document.testcasesForm.WSO2_QAP_TEST_CASE_ID.checked;
+    }
+    return selected;
+}
+
+function deleteTestcase() {
+    var selected = isTestcaseSelected();
+    if (!selected) {
+        CARBON.showInfoDialog('Please select the applications to be deleted.');
+        return;
+    }
+    if (allSelected) {
+        CARBON.showConfirmationDialog("Do you want to delete all products?",
+            function () {
+                //location.href = '../controller/deleteTestcase.jag?deleteAllWebapps=true&webappState=all';
+                document.testcasesForm.action = '../controller/deleteTestcase.jag';
+                document.testcasesForm.submit();
+            }
+        );
+    } else {
+        CARBON.showConfirmationDialog("Do you want to delete the selected products?",
+            function () {
+                document.testcasesForm.action = '../controller/deleteTestcase.jag';
+                document.testcasesForm.submit();
+            }
+        );
+    }
+}
+
+function selectAllInThisPage(isSelected) {
+    allSelected = false;
+    if (document.testcasesForm.WSO2_QAP_TEST_CASE_ID != null &&
+        document.testcasesForm.WSO2_QAP_TEST_CASE_ID[0] != null) { // there is more than 1
+        if (isSelected) {
+            for (var j = 0; j < document.testcasesForm.WSO2_QAP_TEST_CASE_ID.length; j++) {
+                document.testcasesForm.WSO2_QAP_TEST_CASE_ID[j].checked = true;
+            }
+        } else {
+            for (j = 0; j < document.testcasesForm.WSO2_QAP_TEST_CASE_ID.length; j++) {
+                document.testcasesForm.WSO2_QAP_TEST_CASE_ID[j].checked = false;
+            }
+        }
+    } else if (document.testcasesForm.WSO2_QAP_TEST_CASE_ID != null) { // only 1
+        document.testcasesForm.WSO2_QAP_TEST_CASE_ID.checked = isSelected;
+    }
+    return false;
+}
+
+function selectAllInAllPages() {
+    selectAllInThisPage(true);
+    allSelected = true;
+    return false;
+}
+
+function resetVars() {
+    allSelected = false;
+
+    var isSelected = false;
+    if (document.testcasesForm.WSO2_QAP_TEST_CASE_ID[0] != null) { // there is more than 1 sg
+        for (var j = 0; j < document.testcasesForm.WSO2_QAP_TEST_CASE_ID.length; j++) {
+            if (document.testcasesForm.WSO2_QAP_TEST_CASE_ID[j].checked) {
+                isSelected = true;
+            }
+        }
+    } else if (document.testcasesForm.WSO2_QAP_TEST_CASE_ID != null) { // only 1 sg
+        if (document.testcasesForm.WSO2_QAP_TEST_CASE_ID.checked) {
+            isSelected = true;
+        }
+    }
+    return false;
+}
+
