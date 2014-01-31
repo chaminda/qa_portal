8bc40644f0462076557fe815e25192287c75dbf4
dammina <dmsahabandu@gmail.com>
2014-01-31 14:01:55 +0530

    feature list added to version_dashboard

--- resources/js/logic_version_dashboard.js
+++ resources/js/logic_version_dashboard.js
@@ -1,4 +1,5 @@
-var allSelected = false;
+var allBuildsSelected = false;
+var allFeaturesSelected = false;
 
 function isVersionBuildSelected() {
     var selected = false;
@@ -13,32 +14,21 @@ function isVersionBuildSelected() {
     return selected;
 }
 
-function deleteVersionBuilds() {
-    var selected = isVersionBuildSelected();
-    if (!selected) {
-        CARBON.showInfoDialog('Please select the product versions to be deleted.');
-        return;
+function isVersionFeatureSelected() {
+    var selected = false;
+    if (document.featuresForm.featureID[0] != null) { // there is more than 1
+        for (var j = 0; j < document.featuresForm.featureID.length; j++) {
+            selected = document.featuresForm.featureID[j].checked;
+            if (selected) break;
+        }
+    } else if (document.featuresForm.featureID != null) { // only 1
+        selected = document.featuresForm.featureID.checked;
     }
-    if (allSelected) {
-        CARBON.showConfirmationDialog("Do you want to delete all product versions?",
-            function () {
-//                location.href = '../controller/deleteProductVersions.jag?deleteAllWebapps=true&webappState=all';
-                document.productVersionForm.action = '../controller/deleteProductVersions.jag';
-                document.productVersionForm.submit();
-            }
-        );
-    } else {
-        CARBON.showConfirmationDialog("Do you want to delete the selected product versions?",
-            function () {
-                document.productVersionForm.action = '../controller/deleteProductVersions.jag';
-                document.productVersionForm.submit();
-            }
-        );
-    }
+    return selected;
 }
 
 function selectAllInBuildTable(isSelected) {
-    allSelected = false;
+    allBuildsSelected = true;
     if (document.buildsForm.buildId != null &&
         document.buildsForm.buildId[0] != null) { // there is more than 1
         if (isSelected) {
@@ -56,15 +46,75 @@ function selectAllInBuildTable(isSelected) {
     return false;
 }
 
-
-
-
-
-function selectAllInAllPages() {
-    selectAllInThisPage(true);
-    allSelected = true;
+function selectAllInFeatureTable(isSelected) {
+    allFeaturesSelected = true;
+    if (document.featuresForm.featureID != null &&
+        document.featuresForm.featureID[0] != null) { // there is more than 1
+        if (isSelected) {
+            for (var j = 0; j < document.featuresForm.featureID.length; j++) {
+                document.featuresForm.featureID[j].checked = true;
+            }
+        } else {
+            for (j = 0; j < document.featuresForm.featureID.length; j++) {
+                document.featuresForm.featureID[j].checked = false;
+            }
+        }
+    } else if (document.featuresForm.featureID != null) { // only 1
+        document.featuresForm.featureID.checked = isSelected;
+    }
     return false;
 }
+
+function deleteVersionBuilds() {
+    var selected = isVersionBuildSelected();
+    if (!selected) {
+        CARBON.showInfoDialog('Please select the version builds to be deleted.');
+        return;
+    }
+    if (allBuildsSelected) {
+        CARBON.showConfirmationDialog("Do you want to delete all version builds?",
+            function () {
+//                location.href = '../controller/deleteProductVersions.jag?deleteAllWebapps=true&webappState=all';
+                document.buildsForm.action = '../controller/deleteVersionBuilds.jag';
+                document.buildsForm.submit();
+            }
+        );
+    } else {
+        CARBON.showConfirmationDialog("Do you want to delete the selected version builds?",
+            function () {
+                document.buildsForm.action = '../controller/deleteVersionBuilds.jag';
+                document.buildsForm.submit();
+            }
+        );
+    }
+}
+
+function deleteVersionBuilds() {
+    var selected = isVersionFeatureSelected();
+    if (!selected) {
+        CARBON.showInfoDialog('Please select the version feature to be deleted.');
+        return;
+    }
+    if (allFeaturesSelected) {
+        CARBON.showConfirmationDialog("Do you want to delete all version features?",
+            function () {
+//                location.href = '../controller/deleteProductVersions.jag?deleteAllWebapps=true&webappState=all';
+                document.featuresForm.action = '../controller/deleteVersionFeatures.jag';
+                document.featuresForm.submit();
+            }
+        );
+    } else {
+        CARBON.showConfirmationDialog("Do you want to delete the selected version features?",
+            function () {
+                document.featuresForm.action = '../controller/deleteVersionFeatures.jag';
+                document.featuresForm.submit();
+            }
+        );
+    }
+}
+
+
+
 
 function resetVars() {
     allSelected = false;
@@ -85,59 +135,3 @@ function resetVars() {
 }			
 
 
-			function selectAllInTable1(isSelected) {
-                                     allWebappsSelected = false;
-                                     if (document.WSO2_QAP_BUILD_form1.WSO2_QAP_BUILD != null &&
-                                               document.WSO2_QAP_BUILD_form1.WSO2_QAP_BUILD[0] != null) { // there is more than 1
-                                      if (isSelected) {
-                                       for (var j = 0; j < document.WSO2_QAP_BUILD_form1.WSO2_QAP_BUILD.length; j++) {
-                                        document.WSO2_QAP_BUILD_form1.WSO2_QAP_BUILD[j].checked = true;
-                                     }
-                                  } else {
-                                    for (j = 0; j < document.WSO2_QAP_BUILD_form1.WSO2_QAP_BUILD.length; j++) {
-                                     document.WSO2_QAP_BUILD_form1.WSO2_QAP_BUILD[j].checked = false;
-                                  }
-                               }
-                                           } else if (document.WSO2_QAP_BUILD_form1.WSO2_QAP_BUILD != null) { // only 1
-                                            document.WSO2_QAP_BUILD_form1.WSO2_QAP_BUILD.checked = isSelected;
-                                         }
-                                         return false;
-                                      }
-
-                                      function selectAllInTable2(isSelected) {
-                                        allWebappsSelected = false;
-                                        if (document.webappsForm2.webappFileName != null &&
-                                               document.webappsForm2.webappFileName[0] != null) { // there is more than 1
-                                         if (isSelected) {
-                                          for (var j = 0; j < document.webappsForm2.webappFileName.length; j++) {
-                                           document.webappsForm2.webappFileName[j].checked = true;
-                                        }
-                                     } else {
-                                       for (j = 0; j < document.webappsForm2.webappFileName.length; j++) {
-                                        document.webappsForm2.webappFileName[j].checked = false;
-                                     }
-                                  }
-                                           } else if (document.webappsForm2.webappFileName != null) { // only 1
-                                            document.webappsForm2.webappFileName.checked = isSelected;
-                                         }
-                                         return false;
-                                      }
-
-                                      function selectAllInTable3(isSelected) {
-                                        allWebappsSelected = false;
-                                        if (document.webappsForm3.webappFileName != null &&
-                                               document.webappsForm3.webappFileName[0] != null) { // there is more than 1
-                                         if (isSelected) {
-                                          for (var j = 0; j < document.webappsForm3.webappFileName.length; j++) {
-                                           document.webappsForm3.webappFileName[j].checked = true;
-                                        }
-                                     } else {
-                                       for (j = 0; j < document.webappsForm3.webappFileName.length; j++) {
-                                        document.webappsForm3.webappFileName[j].checked = false;
-                                     }
-                                  }
-                                           } else if (document.webappsForm3.webappFileName != null) { // only 1
-                                            document.webappsForm3.webappFileName.checked = isSelected;
-                                         }
-                                         return false;
-                                      }
