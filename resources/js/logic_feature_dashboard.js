44ee2d20f649d0a87823a86e093426202c0ba0fd
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2014-02-04 14:00:04 +0530

    test suits added to feature dashboard

--- resources/js/logic_feature_dashboard.js
+++ resources/js/logic_feature_dashboard.js
@@ -1,5 +1,8 @@
 var allSamplesSelected = false;
+var allTestSuitsSelected = false;
 
+
+//for samples
 function isSampleSelected() {
     var selected = false;
     if (document.SampleForm.sampleId[0] != null) { // there is more than 1
@@ -57,7 +60,7 @@ function deleteSamples() {
 }
 
 
-function resetVars() {
+function resetSampleVars() {
     allSamplesSelected = false;
 
     var isSelected = false;
@@ -74,3 +77,81 @@ function resetVars() {
     }
     return false;
 }
+
+
+//for test suits
+function isTestSuitSelected() {
+    var selected = false;
+    if (document.TestSuitForm.testSuitId[0] != null) { // there is more than 1
+        for (var j = 0; j < document.TestSuitForm.testSuitId.length; j++) {
+            selected = document.TestSuitForm.testSuitId[j].checked;
+            if (selected) break;
+        }
+    } else if (document.TestSuitForm.testSuitId != null) { // only 1
+        selected = document.TestSuitForm.testSuitId.checked;
+    }
+    return selected;
+}
+
+function selectAllInTestSuitTable(isSelected) {
+    allTestSuitsSelected = true;
+    if (document.TestSuitForm.testSuitId != null &&
+        document.TestSuitForm.testSuitId[0] != null) { // there is more than 1
+        if (isSelected) {
+            for (var j = 0; j < document.TestSuitForm.testSuitId.length; j++) {
+                document.TestSuitForm.testSuitId[j].checked = true;
+            }
+        } else {
+            for (j = 0; j < document.TestSuitForm.testSuitId.length; j++) {
+                document.TestSuitForm.testSuitId[j].checked = false;
+            }
+        }
+    } else if (document.TestSuitForm.testSuitId != null) { // only 1
+        document.TestSuitForm.testSuitId.checked = isSelected;
+    }
+    return false;
+}
+
+function deleteTestSuits() {
+    var selected = isTestSuitSelected();
+    if (!selected) {
+        CARBON.showInfoDialog('Please select the sample to be deleted.');
+        return;
+    }
+    if (allSamplesSelected) {
+        CARBON.showConfirmationDialog("Do you want to delete all samples?",
+            function () {
+//                location.href = '../controller/deleteProductVersions.jag?deleteAllWebapps=true&webappState=all';
+                document.TestSuitForm.action = '../controller/deleteTestSuitFeatureDashboard.jag';
+                document.TestSuitForm.submit();
+            }
+        );
+    } else {
+        CARBON.showConfirmationDialog("Do you want to delete the selected samples?",
+            function () {
+                document.TestSuitForm.action = '../controller/deleteTestSuitFeatureDashboard.jag';
+                document.TestSuitForm.submit();
+            }
+        );
+    }
+}
+
+
+function resetTestSuitVars() {
+    allTestSuitsSelected = false;
+
+    var isSelected = false;
+    if (document.TestSuitForm.testSuitId[0] != null) { // there is more than 1 sg
+        for (var j = 0; j < document.TestSuitForm.testSuitId.length; j++) {
+            if (document.TestSuitForm.testSuitId[j].checked) {
+                isSelected = true;
+            }
+        }
+    } else if (document.TestSuitForm.testSuitId != null) { // only 1 sg
+        if (document.TestSuitForm.testSuitId.checked) {
+            isSelected = true;
+        }
+    }
+    return false;
+}
+
